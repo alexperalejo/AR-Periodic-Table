fa4bcc082b95575607eba36d12ad1d9545d892e5
Alex P <143479828+MariaAlexandraPeralejo@users.noreply.github.com>
2026-02-22 23:55:00 -0800

    Add unity project files

---
+++ Assets/TapToPlace.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TapToPlace : MonoBehaviour
+{
+    [SerializeField] GameObject placePrefab;
+
+    ARRaycastManager raycastManager;
+    GameObject spawnedObject;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began) return;
+
+        if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(placePrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+            }
+        }
+    }
+}
